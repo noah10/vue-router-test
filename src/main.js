dff023e60ab6c63b84c253efe554f6aba2b2852a
Noah Mercer <noah.mercer@gmail.com>
2022-04-11 14:12:40 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+// uncomment the next line to demonstrate the problem
+// await router.push('/about')
